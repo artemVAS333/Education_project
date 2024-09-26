52e3357048ff27ac000e9006b92d51a911e8dad8
artem VAS <artem.vas333@gmail.com>
2024-09-26 23:06:55 +0300

    fix menu

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -22,18 +22,18 @@ const toggleMenu = () => {
                 <span></span>
             </div>
         </div>
+        <nav class="header_menu" :class="{ active: open }">
+            <ul class="header_menu_list">
+                <li class="menu_list_item" v-for="route in router.options.routes.filter(route => route.showInMenu)"
+                    :key="route.showInMenu">
+                    <RouterLink :to="route.path" class="menu-item"
+                        :class="{ 'active': route.path === router.currentRoute.value.path }" @click="toggleMenu">
+                        {{ route.label }}
+                    </RouterLink>
+                </li>
+            </ul>
+        </nav>
     </div>
-    <nav class="header_menu" :class="{ active: open }">
-        <ul class="header_menu_list">
-            <li class="menu_list_item" v-for="route in router.options.routes.filter(route => route.showInMenu)"
-                :key="route.showInMenu">
-                <RouterLink :to="route.path" class="menu-item"
-                    :class="{ 'active': route.path === router.currentRoute.value.path }" @click="toggleMenu">
-                    {{ route.label }}
-                </RouterLink>
-            </li>
-        </ul>
-    </nav>
 </template>
 
 <style lang="scss">
